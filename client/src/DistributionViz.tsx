ea170f4ae45659cdadd7dc022328247e71c190a1
Taylor Baldwin <taylorbaldwin@gmail.com>
2022-02-08 18:17:49 -0500

    add DistributionsViz component

---
+++ client/src/DistributionViz.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+type DistributionVizProps = {
+  bucketCounts: number[];
+  bucketValueStart: number; // the starting value of the first bucket
+  bucketSize: number;
+  xScaleExtent?: [number, number];
+  yScaleExtent?: [number, number];
+}
+export default class DistributionViz extends React.Component<DistributionVizProps> {
+  canvasRef = React.createRef<HTMLCanvasElement>();
+  componentDidMount() {
+    this.drawDistribution()
+  }
+  componentDidUpdate() {
+    this.drawDistribution()
+  }
+  drawDistribution() {
+    if (!this.canvasRef.current) return
+    const { width, height } = this.canvasRef.current.getBoundingClientRect()
+    this.canvasRef.current.width = width
+    this.canvasRef.current.height = height
+
+    const { bucketCounts, bucketSize, bucketValueStart } = this.props
+    let xScaleExtent = this.props.xScaleExtent
+    if (!xScaleExtent) {
+      xScaleExtent = [bucketValueStart, bucketValueStart + bucketCounts.length * bucketSize]
+    }
+    let yScaleExtent = this.props.yScaleExtent
+    if (!yScaleExtent) {
+      let maxBucketCount = 0
+      for (const num of bucketCounts) maxBucketCount = Math.max(num, maxBucketCount)
+      yScaleExtent = [0, maxBucketCount]
+    }
+
+    const path = [[0, height]]
+    for (let i = 0; i < bucketCounts.length; i++) {
+      const count = bucketCounts[i]
+      const valueStart = bucketValueStart + i * bucketSize
+      const y = lerp(count, yScaleExtent[0], yScaleExtent[1], height, 0)
+      const x0 = lerp(valueStart, xScaleExtent[0], xScaleExtent[1], 0, width)
+      const x1 = lerp(valueStart + bucketSize, xScaleExtent[0], xScaleExtent[1], 0, width)
+      path.push([x0, y], [x1, y])
+    }
+    path.push([width, height])
+
+    const ctx = this.canvasRef.current!.getContext('2d')!
+    ctx.beginPath()
+    ctx.moveTo(path[0][0], path[0][1])
+    for (const p of path.slice(1)) ctx.lineTo(p[0], p[1])
+    ctx.closePath()
+    ctx.fillStyle = '#bbb'
+    ctx.fill()
+  }
+  render() {
+    return <canvas style={{ height: '100%', width: '100%' }} ref={this.canvasRef} />
+  }
+}
+
+function lerp (
+  val: number,
+  domainMin: number,
+  domainMax: number,
+  rangeMin: number,
+  rangeMax: number
+) {
+  const domainDelta = domainMax - domainMin
+  const rangeDelta = rangeMax - rangeMin
+  return (val - domainMin) / domainDelta * rangeDelta + rangeMin
+}
--- client/src/StationsList.tsx
+++ client/src/StationsList.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import './StationsList.css';
+import React from 'react'
+import './StationsList.css'
+import DistributionViz from './DistributionViz'
 import { query, QueryReturn } from './query'
 import type {
   StationId,
@@ -112,37 +113,18 @@ class Station extends React.Component<StationProps, StationState> {
 }
 
 type TripsHistogramProps = { tripCountByDay: TripCountByDay; maxHourlyTrips: number }
-class TripsHistogram extends React.Component<TripsHistogramProps> {
-  canvasRef = React.createRef<HTMLCanvasElement>();
-  componentDidMount() {
-    const { width, height } = this.canvasRef.current!.getBoundingClientRect()
-    this.canvasRef.current!.width = width
-    this.canvasRef.current!.height = height
-    const avgHourlyTrips = new Array(24).fill(0)
-    for (const dayIdx of Object.keys(this.props.tripCountByDay)) {
-      const hours = this.props.tripCountByDay[Number(dayIdx) as DayOfWeek]
-      for (let i = 0; i < hours.length; i++) avgHourlyTrips[i] += hours[i]
-    }
-    const maxTripCount = this.props.maxHourlyTrips
-    const path = [[0, height]]
-    for (let i = 0; i < avgHourlyTrips.length; i++) {
-      const tripCount = avgHourlyTrips[i]
-      const y = (1 - tripCount / maxTripCount) * height
-      const x0 = i * width / 24
-      const x1 = (i + 1) * width / 24
-      path.push([x0, y], [x1, y])
-    }
-    path.push([width, height])
+function TripsHistogram (props: TripsHistogramProps) {
+  const avgHourlyTrips = new Array(24).fill(0)
+  for (const dayIdx of Object.keys(props.tripCountByDay)) {
+    const hours = props.tripCountByDay[Number(dayIdx) as DayOfWeek]
+    for (let i = 0; i < hours.length; i++) avgHourlyTrips[i] += hours[i]
+  }
 
-    const ctx = this.canvasRef.current!.getContext('2d')!
-    ctx.beginPath()
-    ctx.moveTo(path[0][0], path[0][1])
-    for (const p of path.slice(1)) ctx.lineTo(p[0], p[1])
-    ctx.closePath()
-    ctx.fillStyle = '#bbb'
-    ctx.fill()
-  }
-  render() {
-    return <canvas ref={this.canvasRef} />
-  }
+  return <DistributionViz
+    bucketCounts={avgHourlyTrips}
+    bucketValueStart={0}
+    bucketSize={1}
+    xScaleExtent={[0, 24]}
+    yScaleExtent={[0, props.maxHourlyTrips]}
+  />
 }
