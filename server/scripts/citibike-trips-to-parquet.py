4d6a36c1ab791e4ee5f675ce3035d3300fdd7a6a
Taylor Baldwin <taylorbaldwin@gmail.com>
2022-02-08 17:40:39 -0500

    add distance to citibike trips

--- server/scripts/citibike-trips-to-parquet.py
+++ server/scripts/citibike-trips-to-parquet.py
@@ -1,6 +1,7 @@
 # Usage:
 # python3 citibike-trips-to-parquet.py ./citibike-5M.csv ./citibike-5M.parquet
 
+import math
 import sys
 import os
 import pandas as pd
@@ -25,5 +26,33 @@ schema = {
   'birth_year': 'uint16',
 }
 
+def haversine(lat1, lon1, lat2, lon2):
+  # distance between latitudes
+  # and longitudes
+  dLat = (lat2 - lat1) * math.pi / 180.0
+  dLon = (lon2 - lon1) * math.pi / 180.0
+
+  # convert to radians
+  lat1 = (lat1) * math.pi / 180.0
+  lat2 = (lat2) * math.pi / 180.0
+
+  # apply formulae
+  a = (pow(math.sin(dLat / 2), 2) +
+        pow(math.sin(dLon / 2), 2) *
+            math.cos(lat1) * math.cos(lat2));
+  rad = 6371
+  c = 2 * math.asin(math.sqrt(a))
+  return rad * c
+
 df = pd.read_csv(input_file, names=schema.keys(), dtype=schema, usecols=range(14), parse_dates=['start_time', 'stop_time'])
+
+def get_dist (row):
+  return haversine(
+    row['start_station_latitude'],
+    row['start_station_longitude'],
+    row['end_station_latitude'],
+    row['end_station_longitude']
+  )
+
+df['distance'] = df.apply(lambda row: get_dist(row), axis=1)
 df.to_parquet(output_file)
